6b0d99019e943b9d00a8325f269e085e4cd23899
Lolla <lollal@nih.gov>
2020-04-29 22:17:37 -0400

    Firefox and Chrome

--- CTDC_Automation/Test Suites/Canine_TestSuite.ts
+++ CTDC_Automation/Test Suites/Canine_TestSuite.ts
@@ -11,9 +11,15 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>7f6c435b-0c65-4cb0-aab0-d79ed5e4ec76</testSuiteGuid>
    <testCaseLink>
-      <guid>177e4011-ddcd-4c6f-89c6-9363ee7d4496</guid>
+      <guid>66944eb7-bbe9-4f68-a021-83ad8145a334</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_Canine_1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d34f3e1-5ffe-44fc-a795-26a365877356</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Canine_2</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
