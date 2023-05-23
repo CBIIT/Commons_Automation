c39f2ca90b0cfac86cac670597172df679ea3196
ValentinaEp <valentina.epishina@nih.gov>
2023-05-23 10:09:57 -0400

    test suits

--- Test Suites/ICDC_Glioma.ts
+++ Test Suites/ICDC_Glioma.ts
@@ -11,4 +11,53 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>7556b8df-aa24-4c58-aeac-9d46804326ae</testSuiteGuid>
+   <testCaseLink>
+      <guid>77089ecf-9371-4469-8551-16b6a6c0e7be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC01_Canine_StudyGLIOMA01-StudyType_Breed_Sex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>774c3754-2c8c-40be-ba69-5b961ce8d98f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC02_Canine_StudyGLIOMA01-Bioblank_Breed_NeuteredStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b658920-7a80-4d5d-ae95-1297a2005ce3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC03_Canine_StudyGLIOMA01-StudyParticipat_Diagnosis_Sex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1b167236-2198-47ec-9470-7d03c3e12b6f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC04_Canine_StudyGLIOMA01-Breed_PrimDiseaseSite_NeutStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bbe2cead-2e4d-41ff-a17c-9401de13fcc6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC05_Canine_StudyGLIOMA01-Breed_StagOfDiseas_RespToTreatm</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>28a6b0c4-9f0d-401e-a75f-ad8df3e21844</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC06_Canine_StudyGLIOMA01-StudyType_Bioblank_Diagnosis_Sex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd04258a-861f-4a38-bd72-39cad59c2304</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-GLIOMA/TC07_Canine_StudyGLIOMA01-StudyPartic_PrDisSite_StageOfDis_RespToTreat</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
