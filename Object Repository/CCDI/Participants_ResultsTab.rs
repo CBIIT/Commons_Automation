b795163baa5cf948503e90f2c207af69b6464a38
radhakrishnang2 <radhakrishnang2@host.docker.internal>
2023-09-11 19:39:11 -0700

    ccdi hub baseline in progress 2

---
+++ Object Repository/CCDI/Participants_ResultsTab.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Participants_ResultsTab</name>
+   <tag></tag>
+   <elementGuidId>ff0bfc90-39ac-42bf-8101-9c6019535ca6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;root&quot;]/div[2]/div[2]/div/div[2]/div/div[5]/div/div/button[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;root&quot;]/div[2]/div[2]/div/div[2]/div/div[5]/div/div/button[1]</value>
+      <webElementGuid>391887ae-397a-44e1-b7ce-cfc82b61a4e9</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
