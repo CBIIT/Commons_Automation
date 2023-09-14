ee70db30d51c42f1c3708bb81fa12a060334286d
kallakuriv2 <kallakuriv2@host.docker.internal>
2023-09-14 06:58:45 -0400

    ICDC_ORGANOIDS01_MultiFilter

---
+++ Test Suites/ICDC_ORGANOIDS01_Sowji_Multi.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ICDC_ORGANOIDS01_Sowji_Multi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6ae156de-7173-45fb-97e4-731ef865ce9e</testSuiteGuid>
+</TestSuiteEntity>
