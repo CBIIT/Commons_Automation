a94aab16773a2d0f951115d59777bbe1fa42bda1
radhakrishnang2 <radhakrishnang2@host.docker.internal>
2023-10-13 07:50:04 -0700

    committed icdc organoids suite renamed

--- Test Suites/ICDC_ORGANOIDS.ts
+++ Test Suites/ICDC_ORGANOIDS.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>ICDC_ORGANOIDS01_Sowji_Multi</name>
+   <name>ICDC_ORGANOIDS</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,58 +10,58 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>6ae156de-7173-45fb-97e4-731ef865ce9e</testSuiteGuid>
+   <testSuiteGuid>0d81e88a-ea06-435c-a205-53d338970ab1</testSuiteGuid>
    <testCaseLink>
-      <guid>f2854e1e-0716-429e-acbd-a60b0bf5c9ea</guid>
+      <guid>d85216a4-f226-4720-ad0a-7d3d41b230ce</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC01_Canine_StudyORGANOIDS01-Diagnosis_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>3db4f21b-ed07-4402-ab5e-9e2063d5a115</guid>
+      <guid>44d6a9cb-2acc-4c63-babc-3e5941a1c403</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC02_Canine_StudyORGANOIDS01-Diagnosis-PrimaryDiseaseSite-NeutStatus</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e7e8c398-0940-47c0-af8c-c8456fd1729c</guid>
+      <guid>00ba6fec-9667-433c-8f2f-3c7d9411dc5a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC03_Canine_StudyORGANOIDS01_Diagnosis_StageOfDisease_NeuteredStatus_Breed</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>02f10566-cf6d-47ce-bed5-12004ae8df05</guid>
+      <guid>4e05b159-b1b9-4927-bdba-28887150774c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC04_Canine_StudyORGANOIDS01-SampleType-SamplePathology</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>67afeb09-54e3-4e9d-884e-773f6754cdb2</guid>
+      <guid>10e1aa91-369e-4f1d-9797-20ce3b62a97a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC05_Canine_StudyORGANOIDS01-NeuterStatus-FileType-FileFormat</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>916726d9-031f-4014-82b5-9270ca4d1c98</guid>
+      <guid>e01b2703-be0e-4416-a0f1-be21dca16e94</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC06_Canine_StudyORGANOIDS01-PrimDiseaseSite-SamplePathology-FileType</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>b6261a2b-6e1d-42eb-9136-271873ef9e20</guid>
+      <guid>f1bd1e8d-f767-49bf-a597-1ac5f1a869e3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC07_Canine_StudyORGANOIDS01-StageOfDisease-FileAssoc-FileType</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a9d50477-03f5-48d3-ad71-5b832800b244</guid>
+      <guid>6f39a2d9-54cb-44be-a539-dcf6fc802bdf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-ORGANOIDS01/TC08_Canine_StudyORGANOIDS01-Sex-FileAssoc-FileFormat</testCaseId>
