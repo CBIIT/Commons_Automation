7b41ba9c2f47ac190ddc7b75b5e5f824638505d7
Sofia David <davids3@nih.gov>
2024-03-11 22:59:19 -0400

    phs002517-TC01-10 Testcases

---
+++ Object Repository/CCDI/ExplorePage/Filters/Diagnosis-Facet/Diagnosis/NeoplasmMetastatic_Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>NeoplasmMetastatic_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>77e5e7e7-ad80-49b4-9eaf-1bec15491f14</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Diagnosis_8000/6 : Neoplasm, metastatic&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Diagnosis_8000/6 : Neoplasm, metastatic&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
