dd605742ad1e2bd2e14c5e640fb187ad0c5333ef
Sofia David <davids3@nih.gov>
2024-03-11 22:59:19 -0400

    phs002517-TC01-10 Testcases

---
+++ Object Repository/CCDI/ExplorePage/Filters/Study_Facet/PHS_Accession/phs002517_Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>phs002517_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>39b4e3f7-4738-4653-8286-03db1d9488be</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_dbGaP phs accession_phs002517&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_dbGaP phs accession_phs002517&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
