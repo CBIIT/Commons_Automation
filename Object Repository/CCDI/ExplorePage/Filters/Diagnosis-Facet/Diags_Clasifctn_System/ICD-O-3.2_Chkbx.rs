92af7fd69857914a514b8e93183119dc8d733e20
kallakuriv2 <kallakuriv2@NCI-02243683-L.nci.nih.gov>
2024-03-11 20:39:29 -0400

    CDS & CCDI Test cases as of 03/11

---
+++ Object Repository/CCDI/ExplorePage/Filters/Diagnosis-Facet/Diags_Clasifctn_System/ICD-O-3.2_Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ICD-O-3.2_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>e95c085b-0547-468a-9f67-628d8dc46936</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Diagnosis Classification System_ICD-O-3.2&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Diagnosis Classification System_ICD-O-3.2&quot;]</value>
+      <webElementGuid>f6a40480-bace-474b-9fa8-ad908e9113b1</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
