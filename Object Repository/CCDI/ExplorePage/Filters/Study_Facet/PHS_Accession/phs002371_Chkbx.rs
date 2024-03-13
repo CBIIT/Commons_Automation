11b8171597aaa8700ef9bba8ced1d2d9530262ad
tatekaraa <tatekaraa@NCI-02294167-L.nci.nih.gov>
2024-03-13 16:42:41 -0400

    Committing the changes for the 3 new studies PHSACCESSION scripts phs0001437,phs002371,phs002430

--- Object Repository/CCDI/ExplorePage/Filters/Study_Facet/PHS_Accession/phs002371_Chkbx.rs
+++ Object Repository/CCDI/ExplorePage/Filters/Study_Facet/PHS_Accession/phs002371_Chkbx.rs
@@ -1,13 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>phs003111_Chkbx</name>
+   <name>phs002371_Chkbx</name>
    <tag></tag>
-   <elementGuidId>d91008e3-6166-4ddc-8c87-1330f89d3c21</elementGuidId>
+   <elementGuidId>348d75ab-efa8-4438-8019-1535af037a88</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
          <key>BASIC</key>
-         <value>//*[@id='checkbox_dbGaP phs accession_phs002430']</value>
+         <value>//*[@id='checkbox_dbGaP phs accession_phs002371']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,7 +21,7 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//*[@id='checkbox_dbGaP phs accession_phs002430']</value>
-      <webElementGuid>44f7dc96-79e0-41a3-9013-b7e286b1c2cf</webElementGuid>
+      <value>//*[@id='checkbox_dbGaP phs accession_phs002371']</value>
+      <webElementGuid>d0fff66c-4997-4615-9b4a-bdf08be08aae</webElementGuid>
    </webElementProperties>
 </WebElementEntity>
