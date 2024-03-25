bf9396150f4ee3bd9854bf86eaac70109b23c6f8
kallakuriv2 <kallakuriv2@host.docker.internal>
2024-03-24 20:51:05 -0400

    PHS002371 & PHS002504 - 20 Testcases

---
+++ Object Repository/CCDI/ExplorePage/Filters/SequencingLibrary_Facet/LibraryStrategy/BisulfiteSeq_Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BisulfiteSeq_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>b8f3122f-006e-45a7-8638-f07d2e4aa4ad</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Library strategy_Bisulfite-Seq&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Library strategy_Bisulfite-Seq&quot;]</value>
+      <webElementGuid>b5e5db3d-e46d-481b-8baf-dfb55c392385</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
