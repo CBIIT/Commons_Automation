a429fd4b3b667d50f8b69909a47612243b1deb84
kallakuriv2 <kallakuriv2@host.docker.internal>
2024-03-26 20:13:31 -0400

    Phs002790 study

---
+++ Test Suites/CCDI_phs002790.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CCDI_phs002790</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>58167b23-3d62-4424-a4e7-6024bc3b1e14</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a61a862-5ae8-4079-8d9f-ba6bfdb367a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC01_CCDI_PHS-Accession-PHS-Accession-phs002790</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1e8a7626-78f8-43d9-8575-2de5228178c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC02_CCDI_PHS-Accession-phs002790_Sex-Female_Race-Asian_Ethnicity-HispanicorLatino</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>df843e4b-c094-4aba-ae5e-dd102a26db45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC03_CCDI_PHS-Accession-phs002790_Sex-Male_Race-White_Ethnicity-Unknown</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c0e5082b-a364-496f-9bd4-433a47ddc9fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC04_CCDI_PHS-Accession-phs002790_GrantID_Institution_Acronym_SST</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>819f9d42-25c3-43df-b11b-ab4b701f9fe4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC05_CCDI_PHS-Accession-phs002790_AnatomicSite_Diagnosis_DiagClasSys_DiagVerfStatus_DiagBasis_DiseasePhase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9ef44aa-7ca1-43e9-977a-f24e2998e30b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC06_CCDI_PHS-Accession-phs002790_FileType-fastq_LibSrc-Genomic_LibSel-Hybrid_LibStrat-WXS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cfa55930-4817-453d-8d69-dd627a6b68c1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC07_CCDI_PHS-Accession-phs002790_FileType-bam_LibSrc-Transcriptomic_LibSel-Other_LibStrat-ArcherFusion</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>57ecb9b3-118a-417d-b10b-6f22fad1a3b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Multifilters/phs002790/TC08_CCDI_PHS-Accession-phs002790_Samp-Anatomic-Blood_TumorStatus-Normal_Tumorclass-NotApplicable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
