1c6e24202746a6708914f42a5066a4cf84a571fc
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2024-03-28 19:01:18 -0700

    Update phs001787_Chkbx.rs

--- Object Repository/CDS/Data_page/Filter/StudyFacet/PHS_Accession/phs001787_Chkbx.rs
+++ Object Repository/CDS/Data_page/Filter/StudyFacet/PHS_Accession/phs001787_Chkbx.rs
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>phs001787_Chkbx</name>
+   <name>phs002050_Chkbx</name>
    <tag></tag>
-   <elementGuidId>663f5728-e007-491a-8121-37665a372154</elementGuidId>
+   <elementGuidId>de2fc428-9555-4ca1-9a8f-a01b26ce140e</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//*[@id='checkbox_PHS Accession_phs001787']</value>
+         <value>//*[@id='checkbox_PHS Accession_phs002050']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,7 +17,7 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//*[@id='checkbox_PHS Accession_phs001787']</value>
-      <webElementGuid>9fbcd9e8-4172-40cd-a7b8-542b1ff14ce8</webElementGuid>
+      <value>//*[@id='checkbox_PHS Accession_phs002050']</value>
+      <webElementGuid>99c6c2c4-9cca-45b3-8db6-3f46d40996ba</webElementGuid>
    </webElementProperties>
 </WebElementEntity>
