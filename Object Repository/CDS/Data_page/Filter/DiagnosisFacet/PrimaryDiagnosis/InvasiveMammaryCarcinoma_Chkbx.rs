966e4b6876531d3c3a3180594f9cc3f16c09834e
tatekaraa <tatekaraa@host.docker.internal>
2024-04-16 15:27:25 -0400

    Created scripts for CDS studies phs002479 and phs002790

---
+++ Object Repository/CDS/Data_page/Filter/DiagnosisFacet/PrimaryDiagnosis/InvasiveMammaryCarcinoma_Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>InvasiveMammaryCarcinoma_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>f454a1d7-62dc-4597-8c23-7491469a654c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_PRIMARY DIAGNOSIS_Invasive Mammary Carcinoma&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_PRIMARY DIAGNOSIS_Invasive Mammary Carcinoma&quot;]</value>
+      <webElementGuid>69373f24-6dac-4d95-a02a-d83812d94842</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/CDS/Data_page/Filter/SamplesFacet/SampleType/DNA_Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>DNA_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>55270e1c-9e8c-415d-b32f-b4119fb30fd9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Sample Type_DNA&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Sample Type_DNA&quot;]</value>
+      <webElementGuid>c55e9ad3-7b79-4cf1-a02c-f8943c66bb8c</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
