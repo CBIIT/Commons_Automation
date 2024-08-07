ae5c65c1865d0c8755f544e671f0be4984943261
sohil-nih <sohil.nih@gmail.com>
2024-08-07 14:07:13 -0400

    Integrated ICDC tsv reading functions

---
+++ PythonFiles/CDS/Statbar.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pandasql as ps
+import os
+import Utils
+
+# Get participants query from excel
+statbar_query = Utils.get_value_from_excel("StatQuery")
+print(f'This is Statbar query fitched from input excel:\n {statbar_query}')
+
+# Passing query to dataframe
+result_df_statbar = Utils.df_run_query(statbar_query)   
+
+# Specify the output excel path
+output_excel = os.path.join(os.path.dirname(os.path.abspath(__file__)), Utils.get_output_file_path, Utils.get_value_from_excel('TsvExcel'))
+
+# Write the result DataFrame to an Excel file
+Utils.write_to_excel(output_excel, "StatOutput", result_df_statbar)
+
+# Print output data
+print(f"Statbar data successfully written to: {output_excel}")
+ 
---
+++ PythonFiles/ICDC/CompareData.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pandasql as ps
+import os
+import sys
+import re
+from datetime import datetime
+
+
+# This returns input excel path
+ui_data = sys.argv[1]
+
+# This returns output excel path
+db_data = sys.argv[2]
+
+# This returns node/tsv/txt files path
+print(ui_data)
+print("skdjflkdsjfkldsjfkldsjlkfjdslkfjdslkfj")
+print(db_data)
+
+print("XXxxxxxxxxx xx xxxxxxxxx  No data: The file is empty.")
+       
