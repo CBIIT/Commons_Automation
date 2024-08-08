6d8b7aebaf2bf47292589440acc929a777e914e3
sohil-nih <sohil.nih@gmail.com>
2024-08-08 19:48:34 -0400

    Updated PythonReader class

---
+++ Test Suites/ICDC_Tsv_TestCases.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ICDC_Tsv_TestCases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5adbc746-d8dd-402a-aed6-24946f21c114</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce5fc8d1-9c81-43c4-af78-8e08d5dd5aea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/TLC01/TC01_ICDC_TCL01_Breed-GoldenRetriever</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e89018cd-ab36-4673-9fc6-3b02078f0c77</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC01_ICDC_MGT01_Breed-LabradorRetriever</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
