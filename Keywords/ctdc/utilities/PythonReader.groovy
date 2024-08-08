f434674263dd9595a1d1c59b16ddc51abf46130c
sohil-nih <sohil.nih@gmail.com>
2024-08-08 19:48:34 -0400

    Updated PythonReader class

--- Keywords/ctdc/utilities/PythonReader.groovy
+++ Keywords/ctdc/utilities/PythonReader.groovy
@@ -62,8 +62,8 @@ public class PythonReader {
 	@Keyword
 	public static void readFile(String pythonFileName) {
 
-		String binPath = "/Library/Frameworks/Python.framework/Versions/3.12/bin/python3";
-		//String binPath = "python3";
+		//String binPath = "/Library/Frameworks/Python.framework/Versions/3.12/bin/python3";
+		String binPath = "python3";
 		String inputExcelPath = GlobalVariable.InputExcel;
 		String outputFilePath = Paths.get(usrDir, "OutputFiles")
 		String nodeFilespath = getNodeFilesPath()
@@ -103,54 +103,4 @@ public class PythonReader {
 		}
 	}
 
-
-
-
-	/**
-	 * This function reads and executes a Python file.
-	 * @param pythonFileName The name of the Python file to execute.
-	 */
-	@Keyword
-	public static void compareLists(String pythonFileName, List<List<XSSFCell>> l1, List<List<XSSFCell>> l2) {
-
-		String binPath = "/Library/Frameworks/Python.framework/Versions/3.12/bin/python3";
-		//String binPath = "python3";
-		String inputExcelPath = GlobalVariable.InputExcel;
-		String outputFilePath = Paths.get(usrDir, "OutputFiles")
-		String nodeFilespath = getNodeFilesPath()
-		String pyPath = getPythonPath(pythonFileName);
-
-		try {
-			// Create a process builder
-			ProcessBuilder processBuilder = new ProcessBuilder(binPath, pyPath, pyPath, l1, l2);
-			processBuilder.redirectErrorStream(true);
-			KeywordUtil.logInfo("Executing Python file: " + pythonFileName);
-
-			// Start the process
-			Process process = processBuilder.start();
-
-			// Capture the output
-			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			StringBuilder output = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				output.append(line).append("\n");
-			}
-
-			// Wait for the process to finish and get the exit code
-			int exitCode = process.waitFor();
-
-			// Print the output and handle the exit code
-			if (exitCode == 0) {
-				KeywordUtil.logInfo("Python script executed successfully.");
-				KeywordUtil.logInfo("Script output: " + output.toString());
-			} else {
-				KeywordUtil.markFailed("Script execution failed with exit code: " + exitCode);
-				KeywordUtil.markFailed("Script output: " + output.toString());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			KeywordUtil.markFailed("An error occurred while executing the Python script: " + e.getMessage());
-		}
-	}
 }
