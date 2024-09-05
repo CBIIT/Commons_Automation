25317ca43a688042af5628a997488b9cb88b3ca6
ValentinaEp <valentina.epishina@nih.gov>
2024-09-05 09:30:52 -0400

    test suite

---
+++ Test Suites/ICDC_TSV_OSA03.ts
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ICDC_TSV_OSA03</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ae13ba54-641e-4834-a38a-b8e152f24319</testSuiteGuid>
+   <testCaseLink>
+      <guid>a0f7ec2a-9a63-4089-9b1b-ace54b138775</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC01_ICDC_OSA03_Breed-Boxer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f66bec56-035b-4efa-86ed-8faf68e89b4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC02_ICDC_OSA03_StudyType-Genomics</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>745dc64d-5be4-42f7-b077-63d476a2bb7c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC03_ICDC_OSA03_Biobank-NotApplicable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>30f4cb6b-6f2c-4dc5-9d5b-c8d80e91b3d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC04_ICDC_OSA03_Diagnosis-Osteosarcoma</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a969e5b7-5d72-4970-8ec6-dc7a47b61c3f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC05_ICDC_OSA03_PrimDisSite-Unknown</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c7e9c3a-d3cb-4b22-bbae-26d144b27019</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC06_ICDC_OSA03_StageOfDisease-IIb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>df644451-390f-4f4f-b967-3f2c55c972a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC07_ICDC_OSA03_RespToTreatmt-NotDetermined</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fbe2b5f7-2a36-449a-a641-3c0f0d355d18</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC08_ICDC_OSA03_Sex-Male</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc40503f-fc32-402c-9b93-fa463447df35</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC09_ICDC_OSA03_NeuterStatus-No</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1318c2c6-71b5-4fcb-8901-ad32a289011d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC10_ICDC_OSA03_SampleSite-Bone</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab1b86b5-a657-4f6a-9368-abb60d6686c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC11_ICDC_OSA03_SampleType-PrMaligTumTissue</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a74eea67-1d69-4b51-b54b-051cb811051f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC12_ICDC_OSA03_SamplePatho-OsteoblOsteosarcoma</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>900b7b96-9bb6-4209-88e5-762a4cc35f86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC13_ICDC_OSA03_FileAssociation-Sample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14db9622-6d1b-472f-8a57-7ac59b636228</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC14_ICDC_OSA03_FileType-DNAMethylAnalFile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6b65c75-ea1d-4a64-945d-9243aa21772b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/OSA03/TC15_ICDC_OSA03_FileFormat-bai</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
