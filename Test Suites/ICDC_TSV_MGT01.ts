aa435e5bd82ab67e33afe280bb254f7d64abb21b
sohil-nih <sohil.nih@gmail.com>
2024-09-25 12:14:03 -0400

    Updated Testcase folder structure

--- Test Suites/ICDC_TSV_MGT01.ts
+++ Test Suites/ICDC_TSV_MGT01.ts
@@ -15,105 +15,105 @@
       <guid>6f3d8e8c-7822-4e01-820c-4a79df3a6d55</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC01_ICDC_MGT01_Breed-LabradorRetriever</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC01_ICDC_MGT01_Breed-LabradorRetriever</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>fb2af6ed-2921-4f82-9afa-da0a2aa40de6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC02_ICDC_MGT01_StudyType-Genomics</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC02_ICDC_MGT01_StudyType-Genomics</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>236a5cd9-dcda-4875-bbad-28d984cd62b7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC03_ICDC_MGT01_Biobank-UCD-SVM</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC03_ICDC_MGT01_Biobank-UCD-SVM</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b5855bc3-e287-47c9-b656-484c6adb20b5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC04_ICDC_MGT01_Diagnosis-MammaryCancer</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC04_ICDC_MGT01_Diagnosis-MammaryCancer</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>42491733-4df9-41dc-8532-455d06cfda39</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC05_ICDC_MGT01_PrimDisSite-MammaryGland</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC05_ICDC_MGT01_PrimDisSite-MammaryGland</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>16fe6861-f3f3-46ca-b5a5-8379336780d9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC06_ICDC_MGT01_StageOfDisease-Ia</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC06_ICDC_MGT01_StageOfDisease-Ia</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>c9dc9296-c02a-419e-828d-0969e0be6246</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC07_ICDC_MGT01_RespToTreatmt-NotApplicable</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC07_ICDC_MGT01_RespToTreatmt-NotApplicable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>fd637bde-f57b-4384-ad84-81fc9a6bebd9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC08_ICDC_MGT01_Sex-Female</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC08_ICDC_MGT01_Sex-Female</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8c694118-41c2-46f2-85dc-159e1decd702</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC09_ICDC_MGT01_NeuteredStatus-Yes</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC09_ICDC_MGT01_NeuteredStatus-Yes</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>9695a5e9-493b-4d93-9251-fa63bf6106dc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC10_ICDC_MGT01_SampleSite-MammaryGland</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC10_ICDC_MGT01_SampleSite-MammaryGland</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1d5ad39d-fb89-4383-b432-da2a99f0ca3c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC11_ICDC_MGT01_SampleType-NormalTissue</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC11_ICDC_MGT01_SampleType-NormalTissue</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>6c171892-0772-4209-8045-e84b320ce6dc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC12_ICDC_MGT01_SamplePatho-SimpleCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC12_ICDC_MGT01_SamplePatho-SimpleCarcinoma</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>314473de-1e0b-4d5f-b06c-5bf5fe4b4277</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC13_ICDC_MGT01_FileAssociation-Sample</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC13_ICDC_MGT01_FileAssociation-Sample</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>a74a81fc-65eb-432d-9678-87fdbe307d4d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC14_ICDC_MGT01_FileType-IndexFile</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC14_ICDC_MGT01_FileType-IndexFile</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>3035de10-3c0a-4ac8-a273-42f52641d6d1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Canine_TestCases/TSV_TestCases/MGT01/TC15_ICDC_MGT01_FileFormat-bam</testCaseId>
+      <testCaseId>Test Cases/Canine_TestCases/Studies/MGT01/TC15_ICDC_MGT01_FileFormat-bam</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
