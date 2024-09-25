383be5900fa9e2209be2a31921879c991ea6a007
sohil-nih <sohil.nih@gmail.com>
2024-09-25 13:33:16 -0400

    Removed CDS old input files

---
+++ Scripts/CDS_TestCases/Studies/TC00_QuickRun_Of_TsvFiles/Script1727285112398.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+import java.lang.Runtime
+import java.lang.Process
+import java.lang.ProcessBuilder
+import java.lang.String
+import java.lang.System
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.IOException
+import java.util.stream.Collectors
+import org.python.util.PythonInterpreter
+import java.io.FileReader
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.file.Path
+import java.nio.file.Paths
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.util.KeywordUtil
+import java.nio.file.Paths
+
+
+CustomKeywords.'utilities.TestRunner.RunKatalon'('TC11_CDS_phs001819_InstrumentModel.xlsx')
+
+/*CustomKeywords.'utilities.PythonReader.readFile'('ParticipantsTab.py')
+CustomKeywords.'utilities.PythonReader.readFile'('SamplesTab.py')
+CustomKeywords.'utilities.PythonReader.readFile'('FilesTab.py')*/
+CustomKeywords.'utilities.PythonReader.readFile'('Statbar.py')
+
