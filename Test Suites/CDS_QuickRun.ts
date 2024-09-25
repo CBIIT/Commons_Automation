0108a49b774fad5e4c80bd65180ce75adb961600
sohil-nih <sohil.nih@gmail.com>
2024-09-25 13:38:00 -0400

    Updated CDS Quick run suite

--- Test Suites/CDS_QuickRun.ts
+++ Test Suites/CDS_QuickRun.ts
@@ -15,14 +15,14 @@
       <guid>2b2b9bd6-5178-4eae-b0b3-721905081c7b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/TSV_TestCases/phs002431/TC01_CDS_phs002431_Gender_Female</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs002431/TC01_CDS_phs002431_Gender_Female</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>f1d54ee5-b199-41f5-bb43-cd031167e728</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/TSV_TestCases/phs001287/TC01_CDS_phs001287_Gender_Female</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001287/TC01_CDS_phs001287_Gender_Female</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
