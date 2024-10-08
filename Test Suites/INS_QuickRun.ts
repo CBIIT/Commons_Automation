6ebf751648b98bed4755ec601005be59909d4741
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2024-10-08 11:31:30 -0700

    quick run 2 scripts ins

--- Test Suites/INS_QuickRun.ts
+++ Test Suites/INS_QuickRun.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f4a80565-46db-4381-b700-d1386e7ce549</testSuiteGuid>
+   <testCaseLink>
+      <guid>d7863626-412f-4fc8-ab03-11a6afa14871</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/INS_TestCases/CancerType/TC06_INS_CancerType-ColorectalCancer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8e60d3c8-cc97-4a4d-a228-6765a42bd66e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/INS_TestCases/FocusArea/TC06_INS_FocusArea-SPORE</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
