0a82786bd1a413b497944f3a36614040a1a7a705
ValentinaEp <102980518+ValentinaEp@users.noreply.github.com>
2024-11-05 13:41:52 -0500

    Update Script1727269605401.groovy

--- Scripts/Canine_TestCases/Studies/COTC007B/TC03_ICDC_COTC007B_Breed-BerMountDog/Script1727269605401.groovy
+++ Scripts/Canine_TestCases/Studies/COTC007B/TC03_ICDC_COTC007B_Breed-BerMountDog/Script1727269605401.groovy
@@ -19,7 +19,7 @@ import internal.GlobalVariable as GlobalVariable
 WebUI.closeBrowser()
 
 CustomKeywords.'utilities.TestRunner.RunKatalon'('TC03_ICDC_COTC007B_Breed-BerMountDog.xlsx')
-
+WebUI.waitForElementPresent(findTestObject('Canine/Canine_PopUp_Continue_Btn'), 10)
 CustomKeywords.'utilities.TestRunner.clickTab'('Canine/Canine_PopUp_Continue_Btn')
 System.out.println ("Closed the popup window");
 
