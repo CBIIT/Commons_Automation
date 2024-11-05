f9596d25a18088c9cbd85c802db4b5d98ba9c8cc
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2024-11-05 10:58:04 -0800

    checking ins dataframe issue

--- Test Suites/INS_QuickRun.ts
+++ Test Suites/INS_QuickRun.ts
@@ -14,21 +14,21 @@
    <testCaseLink>
       <guid>d7863626-412f-4fc8-ab03-11a6afa14871</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/INS_TestCases/CancerType/TC06_INS_CancerType-ColorectalCancer</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8e60d3c8-cc97-4a4d-a228-6765a42bd66e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC06_INS_SpecialTopic-SPORE</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>6346b073-bd2e-4f4d-88b0-f2e73a8e3f35</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC01_INS_SpecialTopic-CancerMoonshot</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
