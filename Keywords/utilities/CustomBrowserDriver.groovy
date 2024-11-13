fd0d5711820d24802ea34a776768d6a0f53d2722
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2024-11-13 14:36:50 -0800

    updated user agent to reflect chromedriver version 128

--- Keywords/utilities/CustomBrowserDriver.groovy
+++ Keywords/utilities/CustomBrowserDriver.groovy
@@ -94,7 +94,9 @@ public class CustomBrowserDriver {
 				chromePrefs.put("download.default_directory", manifestPath)
 				chromePrefs.put("download.prompt_for_download", false)
 				//commented the below line as it was suspected to cause page crash errors frequently in INS in data validation framework
-				options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36");
+				options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
+
+				// old version  //options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36");
 				options.setExperimentalOption("useAutomationExtension", false);
 				options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
 			//options.setExperimentalOption("prefs", chromePrefs)
