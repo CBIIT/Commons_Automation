7e2bf513853dcee3bc6de7e050fbb7370a878732
sohil-nih <sohil.nih@gmail.com>
2024-12-03 21:34:47 -0500

    Added phs002599 to Smoke suite

---
+++ Object Repository/C3DC/Filters/Study/dbGaP_Accession/phs002518-Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>phs002518-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>85b1eee6-fcbc-4f76-bbce-c7917b2747c4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_dbGaP ACCESSION_phs002518&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_dbGaP ACCESSION_phs002518&quot;]</value>
+      <webElementGuid>0b41e2d6-7d04-44e1-99eb-8796572f1e5b</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
