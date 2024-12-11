4665f267ef1d4b979476c81a4046803c1ae41f22
sohil-nih <sohil.nih@gmail.com>
2024-12-11 09:33:24 -0500

    Created test scripts for C3DC phs000466

---
+++ Object Repository/C3DC/Filters/Diagnosis/Diagnosis/89643ClearSarckidney-Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>89643ClearSarcKidney-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>af50524b-32cd-4f8d-b677-72829f6e13b9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Diagnosis_8964/3 : Clear cell sarcoma of kidney&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Diagnosis_8964/3 : Clear cell sarcoma of kidney&quot;]</value>
+      <webElementGuid>47ab616b-fc4c-4d8d-9a37-f2e8f5f22513</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
