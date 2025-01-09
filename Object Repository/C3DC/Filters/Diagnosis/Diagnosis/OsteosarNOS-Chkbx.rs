ed73b1cb2c2b0eb9b17628db57a894fe38b61ab2
sohil-nih <sohil.nih@gmail.com>
2025-01-09 13:24:19 -0500

    Added testcases for C3DC phs00468

---
+++ Object Repository/C3DC/Filters/Diagnosis/Diagnosis/OsteosarNOS-Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OsteosarNOS-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>01b64366-9a40-469a-a81b-319539a2e29b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Diagnosis_9180/3 : Osteosarcoma, NOS&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Diagnosis_9180/3 : Osteosarcoma, NOS&quot;]</value>
+      <webElementGuid>84298c5b-a833-4e5b-b8c0-00b1a98b85dd</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
