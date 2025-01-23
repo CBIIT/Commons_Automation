4fcf9d57deaaa7a21d769708004c90c3d36fd8e8
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2025-01-23 14:37:29 -0800

    Enum test scripts for Federation completed.

---
+++ Test Suites/Federation-API_EnumVerification.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Federation-API_EnumVerification</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>97375d5a-c92e-445c-a24f-35e15876f35e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8424bb3-7302-421a-b406-bd723fe02f66</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/File/TC01_EnumVerification_AL-FileCountsByType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7fc4957f-1143-4047-91cf-c94fb0938635</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Subject/TC03_EnumVerification_AL-SubjectCountsByRace</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f0345a68-ab0b-45fd-bbee-9e1d6cc7f78d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Subject/TC02_EnumVerification_AL-SubjectCountsBySex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>35a35d07-89a4-4ddd-a92e-98296d0fc9a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Subject/TC04_EnumVerification_AL-SubjectCountsByVitalStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9890a36c-54e9-4c82-b979-0d0048cf3a6d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Subject/TC01_EnumVerification_AL-SubjectCountsByEthnicity</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ff9cc2c-bdc3-4048-9d5c-4f446339ee0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC03_EnumVerification_AL-SampleCountsByLibraryStrategy</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>594d674f-9f19-4d1e-80e8-b179e6077a84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC08_EnumVerification_AL-SampleCountsByTumorClassification</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5643c754-cbde-4cb6-8aa3-5c50b4edcea0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC02_EnumVerification_AL-SampleCountsByLibrarySelectionMethod</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c5a45fc-f547-4670-b70f-888d09faba66</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC09_EnumVerification_AL-SampleCountsByAnatomicalSites</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed3bc5fc-ab00-4002-ad36-d2e24689c14c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC04_EnumVerification_AL-SampleCountsByLibrarySourceMaterial</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e434927-ce9b-4801-841a-a4e0db6b76c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC05_EnumVerification_AL-SampleCountsByPreservationMethod</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c25ef575-71b2-421b-90c3-4f5055407909</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC07_EnumVerification_AL-SampleCountsByTissueType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b5db11a-ad84-4a2c-a23d-fb4ec5549391</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC06_EnumVerification_AL-SampleCountsBySpecimenMoelcularAnalyteType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ec887a96-d45c-4a5e-9b58-9696ede02a4d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_POC/Federation/Enum_Verification/Sample/TC01_EnumVerification_AL-SampleCountsByDiseasePhase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
