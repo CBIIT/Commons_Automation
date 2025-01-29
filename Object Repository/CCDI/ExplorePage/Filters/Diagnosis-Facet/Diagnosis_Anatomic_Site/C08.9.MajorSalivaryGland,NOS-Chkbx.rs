279b4c43443e5da6f13126e622535b87e8b4b65f
tatekaraa <tatekaraa@NCI-02294167-L.nci.nih.gov>
2025-01-29 14:45:06 -0500

    Committing changes for phs002504

---
+++ Object Repository/CCDI/ExplorePage/Filters/Diagnosis-Facet/Diagnosis_Anatomic_Site/C08.9.MajorSalivaryGland,NOS-Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>C08.9.MajorSalivaryGland,NOS-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>b8688fbe-1865-4633-92ea-35bd346b1e24</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Anatomic Site_C08.9 : Major salivary gland, NOS&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Anatomic Site_C08.9 : Major salivary gland, NOS&quot;]</value>
+      <webElementGuid>e055dd06-2e99-405f-abd2-9126246a2488</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/CCDI/ExplorePage/Filters/Treatment-Facet/TreatmentAgent/Other_Chkbx.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Other_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>4fc7a0bd-934b-4184-ba6f-6694ec50a8de</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Treatment Agent_Other&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Treatment Agent_Other&quot;]</value>
+      <webElementGuid>141a9f34-9d57-46d6-820b-2cb504c4dad5</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
