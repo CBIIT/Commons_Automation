f071fcce18059d83b2bbbe41e203a83800b30f4e
ValentinaEp <valentina.epishina@nih.gov>
2025-02-05 12:34:47 -0500

    UBC03

---
+++ Object Repository/Canine/Filter/SampleSite/BladderMidTrigone_Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BladderMidTrigone_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>12ccc2ee-70f7-4306-a23f-207031b6a279</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Sample Site_Bladder Mid-Trigone&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Sample Site_Bladder Mid-Trigone&quot;]</value>
+      <webElementGuid>13619bd7-19b8-495f-8ece-a74a66b86050</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Canine/Filter/Study/Canine_Filter_Study-UBC03_Chkbx.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>This is the COTB checkbox under Study filter</description>
+   <name>Canine_Filter_Study-UBC03_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>6b3803a8-f02f-41ce-904f-b6713afb2aee</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[contains(text(),'COTC007B')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Study_UBC03 (000010)&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Study_UBC03 (000010)&quot;]</value>
+      <webElementGuid>bc3efe4b-709c-429f-bad9-35b31971de38</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
