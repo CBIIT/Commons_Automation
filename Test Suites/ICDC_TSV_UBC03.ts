cc8842fb107dbac759ad33b9250f171c91e878b3
ValentinaEp <valentina.epishina@nih.gov>
2025-02-05 12:34:47 -0500

    UBC03

---
+++ Test Suites/ICDC_TSV_UBC03.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ICDC_TSV_UBC03</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c2dc3664-b9ea-4372-9834-2cb0fb36431f</testSuiteGuid>
+</TestSuiteEntity>
