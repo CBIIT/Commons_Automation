deecdb62e6f523c9774bb6156623c17571079dfe
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2025-02-06 10:20:22 -0800

    AL subject endpoint 7 scripts for jenkins

---
+++ Object Repository/API/Federation/AggregationLayer/AL_SubjectCountsByDepositions.rs
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>AL_SubjectCountsByDepositions</name>
+   <tag></tag>
+   <elementGuidId>25e82188-01fd-4361-8361-cb4bcd2037f2</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:type</name>
+      <type>Main</type>
+      <value>OAuth 2.0</value>
+      <webElementGuid>49a36668-f2c5-4533-8a3b-c45ae5943153</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer </value>
+      <webElementGuid>5ebc1b51-b1ac-45c9-bc79-29c0748294e9</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:grant_type</name>
+      <type>Main</type>
+      <value>Client Credentials</value>
+      <webElementGuid>cf29ba62-a07b-4970-a6ae-aba9a016af08</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:access_token_url</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>66a6b641-d06e-477c-9622-0b34e09c6563</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:state</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>2ca2ae59-bc6e-42fd-98da-b99702fcc22f</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:authorization_code</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>3c503d94-5b38-4933-927a-7280af1d341d</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:scope</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>22c9f79f-6bfa-4de3-8069-90c5869c70cc</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:access</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>be2ba3b0-2e11-405c-9c27-13254c71630b</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:refesh_token</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>3df3a4c1-d670-425a-a297-387bb5001132</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:oauth_consumer_key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.clientId}</value>
+      <webElementGuid>b5d57e1e-cb35-4a69-93ae-5c1c1cd29884</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:oauth_consumer_secret</name>
+      <type>Main</type>
+      <value>${GlobalVariable.clientSecret}</value>
+      <webElementGuid>f89ae189-5c62-4b20-ad00-6d51d8850cbd</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization:token_type</name>
+      <type>Main</type>
+      <value></value>
+      <webElementGuid>e9e3f34d-e6f8-4416-92e2-992ea16fe560</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>9.7.2</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <path></path>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.baseUrl}/subject/by/depositions/count</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
