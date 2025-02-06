18d52f9f8ed79e32adf73f11448c37e72de920e7
Vincent Leung <vincent.leung@nih.gov>
2025-02-06 13:08:39 -0500

    Add Bento Age suite

--- Test Suites/Bento_QuickRun.ts
+++ Test Suites/Bento_QuickRun.ts
@@ -12,31 +12,10 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>903af0ee-36df-4bab-bb2b-660c17a084cf</testSuiteGuid>
    <testCaseLink>
-      <guid>8ce967fe-dc5e-4bbb-9b85-f10311905e1e</guid>
+      <guid>e3557c9b-b3eb-4b5c-94ab-e692b2fb1ef8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Arm/TC01_Bento_Filter_Arm-A</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>284c3083-7968-4d68-84d3-46290b8acefa</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Arm/TC02_Bento_Filter_Arm-B</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c11164f0-70f8-4155-a4a2-03a32880eca6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Arm/TC03_Bento_Filter_Arm-C</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>871bcf35-9045-46ee-b35f-91ba5c535d7c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Arm/TC04_Bento_Filter_Arm-D</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/Age/TC01_Bento_Filter_Age-10_to_30</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
