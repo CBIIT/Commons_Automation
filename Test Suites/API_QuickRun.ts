8563d7646ada444670cde39729380b88a9acc5c1
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2025-02-06 10:20:22 -0800

    AL subject endpoint 7 scripts for jenkins

--- Test Suites/API_QuickRun.ts
+++ Test Suites/API_QuickRun.ts
@@ -12,10 +12,52 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>fe34ee9e-bf9b-411f-b7ad-9db0635a66b5</testSuiteGuid>
    <testCaseLink>
-      <guid>7ad319a7-226e-432f-8d36-d44529cc895b</guid>
+      <guid>a9fecd78-6601-47a1-85e9-b80e71436c88</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API_POC/Federation/Subject_Counts/TC01_API_Federation_SubjectsCountByEthnicity</testCaseId>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC01_API_Federation_SubjectsCountByEthnicity</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>af67f2bf-58db-45a9-9318-ca980b2ebcb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC02_API_Federation_SubjectsCountByVitalStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4158037-bcfc-46e6-9c3b-8b5c28737068</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC03_API_Federation_SubjectsCountByRace</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ecc8ba5-e9cb-428c-b883-6d07801aa537</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC04_API_Federation_SubjectsCountBySex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>583555ff-b281-4f32-a64f-26d890669c43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC05_API_Federation_SubjectsCountByIdentifiers</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c657288-ea47-46a4-8593-54056e29ba38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC06_API_Federation_SubjectsCountByAgeAtVitalStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08ee6849-3875-4996-8e83-93551acaf6d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Subject_Counts/TC07_API_Federation_SubjectsCountByDepositions</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
