9ee57408f629fcaac8415d08d45c3aaf0dffeed6
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2025-02-07 22:06:51 -0800

    federation api samples - 14 scripts, files - 2 scripts to jenkins

--- Object Repository/API/Federation/AggregationLayer/AL_SampleCounts_by_LibrarySourceMaterial.rs
+++ Object Repository/API/Federation/AggregationLayer/AL_SampleCounts_by_LibrarySourceMaterial.rs
@@ -114,7 +114,7 @@
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://federation-qa.ccdi.cancer.gov/api/v1/sample/by/library_source_material/count</restUrl>
+   <restUrl>${GlobalVariable.baseUrl}/sample/by/library_source_material/count</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
---
+++ Test Suites/API-Federation_Regression.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API-Federation_Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5a6d6e0d-38c7-4144-a095-bde957a16bc1</testSuiteGuid>
+   <testCaseLink>
+      <guid>a9fecd78-6601-47a1-85e9-b80e71436c88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Response_Comparison_AL_vs_Nodes/Subject_Counts/TC01_API_Federation_SubjectsCountByEthnicity</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>af67f2bf-58db-45a9-9318-ca980b2ebcb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Federation/Response_Comparison_AL_vs_Nodes/Subject_Counts/TC02_API_Federation_SubjectsCountByVitalStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
