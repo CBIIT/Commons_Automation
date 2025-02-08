f9224d065c413ac000f1ecb95c2e891af833ce0b
gayathri12aug <40006740+gayathri12aug@users.noreply.github.com>
2025-02-07 22:06:51 -0800

    federation api samples - 14 scripts, files - 2 scripts to jenkins

--- Object Repository/API/Federation/AggregationLayer/AL_SampleCounts_by_SpecimenMolecularAnalyteType.rs
+++ Object Repository/API/Federation/AggregationLayer/AL_SampleCounts_by_SpecimenMolecularAnalyteType.rs
@@ -114,7 +114,7 @@
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://federation-qa.ccdi.cancer.gov/api/v1/sample/by/specimen_molecular_analyte_type/count</restUrl>
+   <restUrl>${GlobalVariable.baseUrl}/sample/by/specimen_molecular_analyte_type/count</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
