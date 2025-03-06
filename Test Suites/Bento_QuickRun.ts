383949c592baabd801279ca4f53968e57257f3c7
Vincent Leung <vincent.leung@nih.gov>
2025-03-06 12:18:57 -0500

    Update Bento_QuickRun

--- Test Suites/Bento_QuickRun.ts
+++ Test Suites/Bento_QuickRun.ts
@@ -12,87 +12,45 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>903af0ee-36df-4bab-bb2b-660c17a084cf</testSuiteGuid>
    <testCaseLink>
-      <guid>94710ebd-fee8-4c4c-92c7-17b95d4ea00b</guid>
+      <guid>24a7c3d9-6678-4763-8d53-798f7baec0f5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC07_Bento_Filter_Diagnosis-MedullaryCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC01_Bento_Filter_EndocrineTherapy-AI</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a948a653-b999-4505-b3e5-fa1dfdf88e18</guid>
+      <guid>d3efbb90-00c0-4871-9116-fe74b331ac64</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC11_Bento_Filter_Diagnosis-PapillaryCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC07_Bento_Filter_EndocrineTherapy-Tam_AI</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>791a6eff-e723-4473-960e-c911571536ef</guid>
+      <guid>49fbbcfe-cbd2-4e67-adb2-95f62bae57a3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC12_Bento_Filter_Diagnosis-TubularCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC06_Bento_Filter_EndocrineTherapy-Tam</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a4f5e661-0374-481b-b33f-173abebbd7c5</guid>
+      <guid>859fc5b4-6ad7-454e-a880-2fa0159c5c8c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC08_Bento_Filter_Diagnosis-MucinousColloidCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC04_Bento_Filter_EndocrineTherapy-OFS_AI</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>89f8f915-62d6-4fe9-a0cb-a41b4e931fb0</guid>
+      <guid>72a7582c-23c5-40b4-b62d-c70d89e4c1fb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC05_Bento_Filter_Diagnosis-InfilDuctalCarcinoma</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC03_Bento_Filter_EndocrineTherapy-OFS</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bcc3c228-a1d0-4511-85dc-8b4a5797c4ad</guid>
+      <guid>594d95dc-d88c-4ace-839b-a64012ce7f21</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC09_Bento_Filter_Diagnosis-NotReported</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>43dd35c0-4cad-4210-a722-74f0cd728128</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC01_Bento_Filter_Diagnosis-Adenocarcinoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b3525dd6-0c4c-4c79-b905-14ce6a8c1417</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC06_Bento_Filter_Diagnosis-InfilLobularCarcinoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>43cdb23d-6d95-48ab-8e4c-c359b13580c5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC03_Bento_Filter_Diagnosis-Comedocarcinoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f0e0da1d-4df3-4fd5-a076-d2883737948b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC04_Bento_Filter_Diagnosis-InfilDuctLobularCarcinoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0114db21-348a-4741-bfd0-01e3758f7e56</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC02_Bento_Filter_Diagnosis-Carcinoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>2c4feb15-a988-42f3-b931-55b5d00a7923</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Bento_TestCases/Diagnosis/TC10_Bento_Filter_Diagnosis-PagetsDisease</testCaseId>
+      <testCaseId>Test Cases/Bento_TestCases/EndocrineTherapy/TC02_Bento_Filter_EndocrineTherapy-None</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
