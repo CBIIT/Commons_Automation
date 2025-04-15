527c85a0afbadbbe634b3c43dbfbde34b3a281fe
kallakuriv2 <kallakuriv2@host.docker.internal>
2025-04-15 10:38:45 -0400

    New changes as of 04/15

---
+++ Object Repository/CDS/Data_page/Filter/DiagnosisFacet/PrimaryDiagnosis/ClearCellCarcinoma_Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ClearCellCarcinoma_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>2a956a18-c1bf-48bc-93f0-e719f691816c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Primary Diagnosis_Clear Cell Carcinoma&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Primary Diagnosis_Clear Cell Carcinoma&quot;]</value>
+      <webElementGuid>6a74b9a5-7470-4d76-8b47-ddcd1934945b</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
