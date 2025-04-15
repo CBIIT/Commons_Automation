140653090298e4165ed6cfdb4fb1c488cce4974e
ValentinaEp <valentina.epishina@nih.gov>
2025-04-15 14:52:02 -0400

    CCDi phs000464

---
+++ Test Suites/CCDI_TSV_phs000464.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CCDI_TSV_phs000464</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8565a54c-8340-4beb-b530-173a3d8e033b</testSuiteGuid>
+   <testCaseLink>
+      <guid>301c30e0-cf37-44c9-915c-63089ee831a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC01_CCDI_phs000464_Diagnos-ActeLeukOfAmbigLinge_DiagAnatSite-Blood</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9dc47e3-480f-47f8-ad83-de5c3680bfd8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC02_CCDI_phs000464_DiagClasfSys-IndicForStudy_Sex-Female-FirstEvent-Progr</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8856c07-a65b-4ffe-bdef-a3287eb36d6f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC03_CCDI_phs000464_DiagBasis-Clinical_Race-NotReported_StudyName</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c83bc86e-a9ed-4c9d-8afb-272088595c55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC04_CCDI_phs000464_DiseasePhase-InitialDiagn_LastKnwnSurvSt-Unknown</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd07051d-a082-4676-837c-cd49ab303ae0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC05_CCDI_phs000464_StudyStatus-Completed_Race-HispanLatino_FirstEvent-Relapse</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
