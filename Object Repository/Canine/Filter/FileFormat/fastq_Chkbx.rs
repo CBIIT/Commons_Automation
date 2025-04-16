d9d1051918196262caf704001ffe250a1862e29c
ValentinaEp <valentina.epishina@nih.gov>
2025-04-16 13:21:19 -0400

    ICDC - OSA04, CCDI - phs000463, phs000464

---
+++ Object Repository/Canine/Filter/FileFormat/fastq_Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>fastq_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>63c864d3-d6e6-4aba-a5b0-3d309c87c9f3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_File Format_fastq&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_File Format_fastq&quot;]</value>
+      <webElementGuid>e93367c7-5f6b-410a-9cc1-b2174ba27736</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
