a029dfe83ef59110b4e3ef9ab9351597b26184fd
Vincent Leung <vincent.leung@nih.gov>
2025-04-24 15:46:56 -0400

    Add TC01, TC02

---
+++ Object Repository/CCDI/ExplorePage/Filters/Treatment-Facet/Treatment-Facet.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Treatment-Facet</name>
+   <tag></tag>
+   <elementGuidId>cf0e8dc1-5d42-4509-8e89-72b79ff2e65a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>(//*[@id=&quot;[object Object]&quot;])[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>(//*[@id=&quot;[object Object]&quot;])[3]</value>
+      <webElementGuid>3bfed32f-5227-402e-afea-59708838e160</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/CCDI/ExplorePage/Filters/Treatment-Facet/TreatmentAgent/TreatmentAgent-Ddn.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>TreatmentAgent-Ddn</name>
+   <tag></tag>
+   <elementGuidId>ee0149a4-9078-4ed7-a7a5-4dcd2fe51917</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;Treatment Agent&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;Treatment Agent&quot;]</value>
+      <webElementGuid>f18d884c-5957-4fff-a053-251337534a6f</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
