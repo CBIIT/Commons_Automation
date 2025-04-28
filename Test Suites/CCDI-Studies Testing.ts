248fcc6df18a2334faf0c827c84cf125f52bfe95
Vincent Leung <vincent.leung@nih.gov>
2025-04-28 11:40:26 -0400

    Fix suite

--- Test Suites/CCDI-Studies Testing.ts
+++ Test Suites/CCDI-Studies Testing.ts
@@ -221,30 +221,32 @@
       <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000720/TC05_CCDI_phs000720_Diag-8900-3-Rhabdomyosarcoma_Lastknwnsrv-Notreported</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
-   <guid>8ee7d6fd-223a-43b0-81c0-5270a8375bdd</guid>
-   <isReuseDriver>false</isReuseDriver>
-   <isRun>true</isRun>
-   <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC01_CCDI_phs002431_Sex-Female_Treatagent-Azacitidine_Filetype-cramindex_Libstratg-Targetedcap</testCaseId>
-   <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-</testCaseLink>
-<testCaseLink>
-   <guid>f4d0ba07-9c3d-4cd3-8b46-a90ac9cbaac2</guid>
-   <isReuseDriver>false</isReuseDriver>
-   <isRun>true</isRun>
-   <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC02_CCDI_phs002431_Race-Asian_Lastknwnsrv-Alive_Sampleanatste-C44.9skinNOS_Datacat-Gener_Filtyp-tbi</testCaseId>
-   <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-</testCaseLink>
-<testCaseLink>
-   <guid>d8e454cf-ea65-4535-8289-aa19f1455f54</guid>
-   <isReuseDriver>false</isReuseDriver>
-   <isRun>true</isRun>
-   <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC03_CCDI_phs002431_Diag-80203-3CarcinomaundiffNOS_Treatagnt-Gemcit_Smpltumrstat-Tumor_Tumrclass-Metastatic</testCaseId>
-   <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-</testCaseLink>
-<testCaseLink>
-   <guid>b7902ccd-7e6a-4f7f-a3c5-de69afc30af1</guid>
-   <isReuseDriver>false</isReuseDriver>
-   <isRun>true</isRun>
-   <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC04_CCDI_phs002431_SampleAnatomicsite-Buccalmucosa</testCaseId>
-</testCaseLink>
+   <testCaseLink>
+      <guid>8ee7d6fd-223a-43b0-81c0-5270a8375bdd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC01_CCDI_phs002431_Sex-Female_Treatagent-Azacitidine_Filetype-cramindex_Libstratg-Targetedcap</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4d0ba07-9c3d-4cd3-8b46-a90ac9cbaac2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC02_CCDI_phs002431_Race-Asian_Lastknwnsrv-Alive_Sampleanatste-C44.9skinNOS_Datacat-Gener_Filtyp-tbi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8e454cf-ea65-4535-8289-aa19f1455f54</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC03_CCDI_phs002431_Diag-80203-3CarcinomaundiffNOS_Treatagnt-Gemcit_Smpltumrstat-Tumor_Tumrclass-Metastatic</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7902ccd-7e6a-4f7f-a3c5-de69afc30af1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002431/TC04_CCDI_phs002431_SampleAnatomicsite-Buccalmucosa</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
