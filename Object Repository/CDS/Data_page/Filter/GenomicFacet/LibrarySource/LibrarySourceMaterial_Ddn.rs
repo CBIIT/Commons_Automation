ccc32c5768c3dad330ef0664c68404b6b1e9140c
Vincent Leung <vincent.leung@nih.gov>
2025-05-08 13:01:31 -0400

    Add phs001524

--- Object Repository/CDS/Data_page/Filter/GenomicFacet/LibrarySource/LibrarySourceMaterial_Ddn.rs
+++ Object Repository/CDS/Data_page/Filter/GenomicFacet/LibrarySource/LibrarySourceMaterial_Ddn.rs
@@ -1,17 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>HybridSelection_Chkbx</name>
+   <name>LibrarySourceMaterial_Ddn</name>
    <tag></tag>
-   <elementGuidId>5d3c9150-c365-48ec-a60b-e1de2262eff3</elementGuidId>
+   <elementGuidId>dc203e43-5e5c-419e-a484-1ac1b32e2b1b</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value></value>
-      </entry>
-      <entry>
          <key>BASIC</key>
-         <value>//*[@id=&quot;checkbox_Library Selection_Hybrid Selection&quot;]</value>
+         <value>//*[@id=&quot;Library Source Material&quot;]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -22,7 +18,7 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//*[@id=&quot;checkbox_Library Selection_Hybrid Selection&quot;]</value>
-      <webElementGuid>34f2fbb9-47a6-4252-984a-2448f030df6b</webElementGuid>
+      <value>//*[@id=&quot;Library Source Material&quot;]</value>
+      <webElementGuid>b9e446dc-f17b-429c-820f-b7b45a6a2558</webElementGuid>
    </webElementProperties>
 </WebElementEntity>
--- Test Suites/CDS_QuickRun.ts
+++ Test Suites/CDS_QuickRun.ts
@@ -12,17 +12,45 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0d23efa7-06b9-4132-88ea-c6f93af1c3b6</testSuiteGuid>
    <testCaseLink>
-      <guid>2b2b9bd6-5178-4eae-b0b3-721905081c7b</guid>
+      <guid>7b92c31c-5475-456d-aa0f-f7a60a521dff</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs002431/TC01_CDS_phs002431_Gender_Female</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC03_CDS_phs001524_FileType-CRAM_Platform-Illumina_Instrument-Nova6000</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f1d54ee5-b199-41f5-bb43-cd031167e728</guid>
+      <guid>33ab6ba0-5182-4b04-ad03-9c56a761d5e2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001287/TC02_CDS_phs001287_Gender_Female</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC04_CDS_phs001524_RefGenAsbly-GRCh37_PrimDiag-Control_ImageModal-NSD</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f64c4ebb-ccb5-459b-aebb-7aed1ba8d551</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC02_CDS_phs001524_SampleTumorStatus-NSD_SampleType-Blood</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44493534-8af5-4be4-a859-860ebfbe79d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC05_CDS_phs001524_LibraryStrategy-TgtCap_LibrarySource-Genomic</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77f4881f-7e8c-44be-bc69-14f54c717b75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC06_CDS_phs001524_LibraryLayout-PairedEnd_LibrarySelection-HybridSelection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>713a6ada-dde9-4258-8f05-28680613819c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC01_CDS_phs001524_Gender-Male_ExperimentStrat-DNASeq</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
