15581a42f379e2f1454b7cfc3341f12c71428d28
Vincent Leung <vincent.leung@nih.gov>
2025-05-15 12:57:54 -0400

    Complete CDS phs001437

--- Test Suites/CDS_QuickRun.ts
+++ Test Suites/CDS_QuickRun.ts
@@ -5,94 +5,45 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>1</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0d23efa7-06b9-4132-88ea-c6f93af1c3b6</testSuiteGuid>
    <testCaseLink>
-      <guid>7b92c31c-5475-456d-aa0f-f7a60a521dff</guid>
+      <guid>bf55a201-1065-4351-a0dc-123d1a82148b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC03_CDS_phs001524_FileType-CRAM_Platform-Illumina_Instrument-Nova6000</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001437/TC03_CDS_phs001437_Sex-Female_FileType-PDF</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>33ab6ba0-5182-4b04-ad03-9c56a761d5e2</guid>
+      <guid>9dc0b889-f8f1-435d-bcfd-10a2f7272400</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC04_CDS_phs001524_RefGenAsbly-GRCh37_PrimDiag-Control_ImageModal-NSD</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001437/TC04_CDS_phs001437_LibrarySource-Transcriptomic_LibraryLayout-PairedEnd</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f64c4ebb-ccb5-459b-aebb-7aed1ba8d551</guid>
+      <guid>416724a2-ae41-4f1a-adc8-135595f1e0ce</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC02_CDS_phs001524_SampleTumorStatus-NSD_SampleType-Blood</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001437/TC01_CDS_phs001437_Sex-Unknown_ExperimentalStrategy-RNASeq</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>44493534-8af5-4be4-a859-860ebfbe79d2</guid>
+      <guid>92743f58-96cd-4b18-8c25-34ec4ad29d8a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC05_CDS_phs001524_LibraryStrategy-TgtCap_LibrarySource-Genomic</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001437/TC05_CDS_phs001437_RefGenAsbly-GRCh38_PrimDiag-CPNET</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>77f4881f-7e8c-44be-bc69-14f54c717b75</guid>
+      <guid>b190af09-08d6-46b3-aa23-344e3b7b0597</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC06_CDS_phs001524_LibraryLayout-PairedEnd_LibrarySelection-HybridSelection</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>713a6ada-dde9-4258-8f05-28680613819c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001524/TC01_CDS_phs001524_Gender-Male_ExperimentStrat-DNASeq</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>609c7aae-1db6-41ab-8abb-f7da3908fdb3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC04_CDS_phs001554_RefGenAsbly-GRCh37_PrimDiag_ColoCarc</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c730e4f8-32a1-408e-a367-fa1bf6330293</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC01_CDS_phs001554_Gender-Female_ExperimentStrat-GWA</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>3bdcb987-9483-4cde-8663-a546c6937872</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC03_CDS_phs001554_FileType-CRAI_Platform-Illumina_Instrument-HiSeq2500</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>aeb32e85-57e2-4963-8f2e-4b7f55c506fc</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC06_CDS_phs001554_LibraryLayout-PairedEnd_LibrarySelection-Random</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b73a6911-06c3-4e71-88eb-d5c807e64720</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC02_CDS_phs001554_SampleTumorStatus-Normal_SampleType-DNA</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d0a17231-2281-4aeb-93a0-0f4d82415722</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001554/TC05_CDS_phs001554_LibraryStrategy-WGS_LibrarySource-Genomic</testCaseId>
+      <testCaseId>Test Cases/CDS_TestCases/Studies/phs001437/TC02_CDS_phs001437_SampleTumorStatus-Normal_SampleType-RNA</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
