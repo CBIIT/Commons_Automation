962589da1939e21fd5ed20a9f7d7f1d36f7c4462
Vincent Leung <vincent.leung@nih.gov>
2025-05-16 12:36:46 -0400

    PBAC WIP and suite

---
+++ Test Suites/CRDC_PBAC.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CRDC_PBAC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c67db095-136b-48b5-a57d-b064b55954d1</testSuiteGuid>
+   <testCaseLink>
+      <guid>a3ca4566-c5a0-4743-a79b-119fb87dfadd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRDC_TestCases/Pbac/TC02_CRDC-PbacDefaultPermissions-Dcp</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8458b196-6676-439a-898d-35096f182ff9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRDC_TestCases/Pbac/TC01_CRDC-PbacDefaultPermissions-Fedlead</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5275f9ea-0532-4844-8fc8-9219ad88a751</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRDC_TestCases/Pbac/TC03_CRDC-PbacDefaultPermissions-Admin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6265c2cc-6acb-4484-b581-61c550abcbb2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRDC_TestCases/Pbac/TC04_CRDC-PbacDefaultPermissions-Submitter</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
