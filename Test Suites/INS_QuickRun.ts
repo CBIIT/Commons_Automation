d528501fa1d5ea4d576b43a6340636fefde541c7
sohil-nih <sohil.nih@gmail.com>
2025-05-21 15:01:49 -0400

    Added INS Datasets page script

--- Test Suites/INS_QuickRun.ts
+++ Test Suites/INS_QuickRun.ts
@@ -12,94 +12,10 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f4a80565-46db-4381-b700-d1386e7ce549</testSuiteGuid>
    <testCaseLink>
-      <guid>d7863626-412f-4fc8-ab03-11a6afa14871</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC06_INS_CancerType-ColorectalCancer</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>8e60d3c8-cc97-4a4d-a228-6765a42bd66e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC06_INS_SpecialTopic-SPORE</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6346b073-bd2e-4f4d-88b0-f2e73a8e3f35</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC01_INS_SpecialTopic-CancerMoonshot</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>fcec5268-07d6-4a96-9122-fe3f3aa89ab3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC02_INS_SpecialTopic-HealthDisparities</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>eeb0e399-262e-4252-ba49-72e52acb6beb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC03_INS_SpecialTopic-HIV_AIDS</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>492336ad-126e-4a63-8ed8-f9bb237e0870</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC04_INS_SpecialTopic-General</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d9193c10-32a7-4450-8cc1-60214c3a0778</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/SpecialTopic/TC05_INS_SpecialTopic-PediatricAYA</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6a94f2e2-a465-4f5f-a79a-f101397db6f3</guid>
+      <guid>0c7a5b77-e8ca-41c2-9097-7d17e82c6289</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC18_INS_CancerType-HeadandNeckCancer</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a98fdcba-edbb-4816-a806-e9388b4bc681</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC19_INS_CancerType-Melanoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>40d50212-7236-49f1-98d5-775bcec6c70c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC20_INS_CancerType-MultipleCancerTypes</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>3c800eab-02d7-4b65-80af-30705efaeec5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC21_INS_CancerType-ProstateCancer</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ca0ffa71-f4d4-4242-96a9-7c8820ff35d2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC22_INS_CancerType-Sarcoma</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>220390d1-18e9-49ae-832f-f9d47f2bb12a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/INS_TestCases/CancerType/TC23_INS_CancerType-SkinCancer</testCaseId>
+      <testCaseId>Test Cases/INS_TestCases/DataSets/TC01_INS_DataSets-EntirePage</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
