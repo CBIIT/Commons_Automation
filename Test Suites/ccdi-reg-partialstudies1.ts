bf9e2b39d3b862a0f8f863a1c79bb8f71a48ff93
tatekaraa <tatekaraa@NCI-02294167-L.nci.nih.gov>
2025-06-25 14:00:14 -0400

    committing test suite1

---
+++ Test Suites/ccdi-reg-partialstudies1.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ccdi-reg-partialstudies1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fc0811b6-49b1-46ee-a649-8b72dfa6e50b</testSuiteGuid>
+   <testCaseLink>
+      <guid>b940e828-f18f-4a07-806e-40cf3b947640</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000463/TC01_CCDI_phs000463_Diagnosis-ActeLymphoblsLeukNOS_Sex-Male</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3eddc3cc-203d-4dd9-967f-c90b60121098</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000463/TC02_CCDI_phs000463_DiagAnatSite-C42.0Blood_FirsEvent-Death</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c144fe8d-2e83-4117-b9ba-8506ddcdbbd4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000463/TC03_CCDI_phs000463_DiagClasSyst-IndictnForStudy_DiagBas-Clinical</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a2b964b1-d898-4708-9bf6-056eba4feaf7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000463/TC04_CCDI_phs000463_StudyName_Race-NatveHawnOrOthrPacifIslnder</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>12bebfdb-9134-4023-b5d0-d184c0714ee4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000463/TC05_CCDI_phs000463_StudyStatus-Completed_LastKnowSurvSt-Alive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a5c7f3bb-ad21-4e05-aa95-befeb0002aae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC01_CCDI_phs000464_Diagnos-ActeLeukOfAmbigLinge_DiagAnatSite-Blood</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f28f3ca-af2a-4e0c-ae2a-cf8bdb408af7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC02_CCDI_phs000464_DiagClasfSys-IndicForStudy_Sex-Female-FirstEvent-Progr</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b33238e2-94d4-4093-ab47-2307fa1b32bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC03_CCDI_phs000464_DiagBasis-Clinical_Race-NotReported_StudyName</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ba37998-faef-45a8-ba08-75ad2f7fb37a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC04_CCDI_phs000464_DiseasePhase-InitialDiagn_LastKnwnSurvSt-Unknown</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>98c622a9-4018-4932-867e-a4b175ec08d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000464/TC05_CCDI_phs000464_StudyStatus-Completed_Race-HispanLatino_FirstEvent-Relapse</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8599cae7-c493-4106-a56c-c42d8d4c8e4c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000465/TC01_CCDI_phs000465_Diagnos-ActeMyelLeuk_SampleAnatcSite-Blood-StudyStatus</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>903ff173-6aa1-4199-8433-5fc438c1793f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000465/TC02_CCDI_phs000465_DiagAnatSite-C42Blood_Race-White-FirstEvent-Death</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c40cec62-2136-4cca-a7ff-4755db955afa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000465/TC03_CCDI_phs000465_DiagClasSyst-IndicForStudy_DiagBasis-Clinical-SampleTumSt-Tumor</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>10c02d6c-3a5e-403e-9c0a-6363b8bf37cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000465/TC04_CCDI_phs000465_DiseasePhase-InitialDiagnosis_LastKnwnSurvSt-Dead_TumClass-Primary</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f169710-8ec4-428c-b2ad-c912f754d6aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000465/TC05_CCDI_phs000465_Sex-Female_LastKnwnSurvSt-Alive_SampleAnatSite-BoneMarrow_StudyName</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>09962a69-c685-42c3-9a4f-2d85eec8883b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000466/TC01_CCDI_phs000466_SexAtBirth-Male</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0aae576a-2024-43ac-a933-9a4eba744700</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000466/TC02_CCDI_phs000466_Race-White_TumorClassif-Primary</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>02ffce43-5dc8-46d4-abf2-f1aedb0c0290</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000466/TC03_CCDI_phs000466_SmplAnatomicst-NotRprtd_Lastknwnsrvlstats-Alive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a6057d2-672f-4d08-8813-35ce0a966b19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000466/TC04_CCDI_phs000466_Diagnosis-89643clearcellsarcoma_Race-Blkafrcamerican_Lastknwnsrvl-Alive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9e030b2d-e7b2-4b43-b3c3-92eb7a938bf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000466/TC05_CCDI_phs000466_Sex-Male_Diagnosis-Basis-Clinical</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2695189f-7228-4440-98b9-124f73e9fe83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000467/TC01_CCDI_phs000467_Sex-Female_Race-Hisplatin_Firstevnt-Progr_Lastknwn-Alive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bba56ca0-da8d-4353-9a5d-69aa5044c99b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000467/TC02_CCDI_phs000467_Sex-Male_Diagn-Ganlionrblstoma_DiagAnatSite-C48RetroPeritoneum</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7b7a7f1-a227-4b1d-8ed8-280e8bc821e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000467/TC02_CCDI_phs000467_Sex-Male_Firstevenr-secmalignantneopl</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8b8b14d-877a-4239-a8bd-2ef78d0dcc5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs000467/TC04_CCDI_phs000467_Race-Unknown_StudyStatus-Completed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/phs002790 phs002504.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>phs002790 phs002504</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6cdf100a-569b-43a6-ab94-94437638e437</testSuiteGuid>
+   <testCaseLink>
+      <guid>e3a78f6b-cb1d-440b-87ae-19c31b4552c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002790/TC01_CCDI_phs002790_Sex-Male_Race-NtiveHawaothrPacific_Datacat-COGClinical</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>864cc481-6ad2-414d-8765-3816a5447a20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002790/TC02_CCDI_phs002790_Sex-Female_TrtmntAgnt-CBDCA_LastknwnSurv-Dead</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8139ac07-ff90-43ed-8745-8730a32b3d84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002790/TC03_CCDI_phs002790_Sex-Female_TrtmntType-Celltherp_Sampleanatmcsite-C72.9cns_Datacat-Clinical</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ca1bbe69-b049-42de-b947-d249f3803631</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002790/TC04_CCDI_phs002790_Filetype-bai_LibSelct-Other_LibSrcMaterial-Bulkcells</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>448eebc0-5fee-4162-bcda-017fa34d0083</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002790/TC05_CCDI_phs002790_Race-NtReported_Diagclass-ICD03.2_LibStart-WXS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b40deb23-2921-4b5d-8a04-c282b50eb003</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002504/TC02_CCDI_phs002504_Sex-Female_Race-Unknown_Trt-Chemotherapy</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5a229c1-3498-4af2-9531-22f6424ab141</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002504/TC03_CCDI_phs002504_Diag-JueviMLukemia_Anat-C42Blood_DiagBasis-Clinical_DataCat-MethylArray</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e31971d-dd78-45cf-9137-09e5169ad6c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002504/TC04_CCDI_phs002504_Sex-Female_Race-Unknown_SmpTumor-Normal_SmpAnt-Blood_Datacat-Sequencing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a987614-86ae-4349-a56f-c374efb911b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002504/TC05_CCDI_phs002504_FileType-bam_LibStr-WXS_LibSel-PCR_DC-Sequencing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a5e763a-cb00-4cf7-93f9-071eaa3b68ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CCDI_TestCases/Studies/phs002504/TC06_CCDI_phs002504_Sex-Female_TrtAgt-other_FileType-fastq_DataCat-Sequencing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
