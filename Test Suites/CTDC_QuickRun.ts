4f3538c463ed842d04c9e4ecccb178a40418cfbe
sohil-nih <sohil.nih@gmail.com>
2025-10-27 13:25:27 -0400

    Created QuickRun suite for CTDC

---
+++ Test Suites/CTDC_QuickRun.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CTDC_QuickRun</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>124e77a4-b63c-4830-9d92-a9213f8bf997</testSuiteGuid>
+   <testCaseLink>
+      <guid>937cdf64-3359-49e7-8f93-44ac50518d55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CTDC_TestCases/Sex/TC01_CTDC_Sex-Male</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1688677d-731c-48ef-a282-0e5a76152811</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CTDC_TestCases/Sex/TC02_CTDC_Sex-Female</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
