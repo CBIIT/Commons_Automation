fbc21b8684dd5ea07680220b8d0e780d4771dc5d
Vincent Leung <vincent.leung@nih.gov>
2026-03-20 12:36:05 -0400

    Add scenarios to regression for tag entities

--- Scripts/API/MDB/STS/Tags/TC03_API_MDB-STS_TagKeyValueEntities/Script1774100000001.groovy
+++ Scripts/API/MDB/STS/Tags/TC03_API_MDB-STS_TagKeyValueEntities/Script1774100000001.groovy
@@ -14,6 +14,13 @@ MDB_Parity.verifyTagKeyValueEntitiesParity('Category', 'administrative')
 //Test for "type": "Term" -- failing currently because of bug DATATEAM-496
 MDB_Parity.verifyTagKeyValueEntitiesParity('Added', '01-20-26')
 MDB_Parity.verifyTagKeyValueEntitiesParity('Added', '12-23-25')
+MDB_Parity.verifyTagKeyValueEntitiesParity('Provenance', 'CMB')
+MDB_Parity.verifyTagKeyValueEntitiesParity('Labeled', 'Study Type')
+MDB_Parity.verifyTagKeyValueEntitiesParity('Provenance', '12-23-25')
+//these two have both types Term and Property and behavior is present here also for the Term
+MDB_Parity.verifyTagKeyValueEntitiesParity('Labeled', 'Description')
+MDB_Parity.verifyTagKeyValueEntitiesParity('Labeled', 'Study Code, Assigned to Study')
+
 
 //Test for "type": "Concept"
 MDB_Parity.verifyTagKeyValueEntitiesParity('mapping_source', 'alternate_name')
