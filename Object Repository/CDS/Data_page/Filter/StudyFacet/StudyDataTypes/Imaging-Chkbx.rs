89b1b1ebd2600a69d461a79e64ce43691962096e
yufan122 <jadefan1307@gmail.com>
2026-03-30 16:31:37 -0400

    New CDS test cases

---
+++ Object Repository/CDS/Data_page/Filter/StudyFacet/StudyDataTypes/Imaging-Chkbx.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Imaging-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>7b8f1d55-573f-425b-b694-dce560956198</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Study Data Types_Imaging&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Study Data Types_Imaging&quot;]</value>
+      <webElementGuid>4261a619-b103-4f88-aaae-f6d4d0a2eedf</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
