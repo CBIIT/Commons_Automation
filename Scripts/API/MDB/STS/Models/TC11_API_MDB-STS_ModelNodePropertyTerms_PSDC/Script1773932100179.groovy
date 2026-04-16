acfde10c886c28c325964e72805ce533e08a9d94
Vincent Leung <vincent.leung@nih.gov>
2026-04-16 09:16:12 -0400

    Handle stage runs for MDB tests

--- Scripts/API/MDB/STS/Models/TC11_API_MDB-STS_ModelNodePropertyTerms_PSDC/Script1773932100179.groovy
+++ Scripts/API/MDB/STS/Models/TC11_API_MDB-STS_ModelNodePropertyTerms_PSDC/Script1773932100179.groovy
@@ -4,4 +4,4 @@ import utilities.MDB_Parity
 KeywordUtil.logInfo("Running TC05: Property terms parity for models for latest version - /model/:modelHandle/version/:versionString/node/:nodeHandle/property/:propHandle/terms/ endpoint")
   
 //Leave array empty for all models or add specific models to test: 'CCDI', 'CDS'
-MDB_Parity.verifyAllModelsNodePropertyTermsParityLatest(['ICDC'])
+MDB_Parity.verifyAllModelsNodePropertyTermsParityLatest(['PSDC'])
